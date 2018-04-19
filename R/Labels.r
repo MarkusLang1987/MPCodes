8b6ff8cfdf78d1342887a984bf92ad8270577611
MarkusLang1987 <langmarkus@hotmail.com>
2018-04-19 11:23:49 +0200

    Update Labels.r

--- R/Labels.r
+++ R/Labels.r
@@ -1,7 +1,7 @@
 mp_belabelung <- function(data) {
     namelist <- list()
-    for(i in 1:ncol(data2)) {
-        data <- as.data.frame(unique(data2[,i]))
+    for(i in 1:ncol(data)) {
+        data <- as.data.frame(unique(data[,i]))
         colnames(data) <- NULL
         namelist[[i]] <- data
     }
